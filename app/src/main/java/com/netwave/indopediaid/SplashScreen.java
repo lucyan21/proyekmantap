4a9e898dbccac389b66bebb148dbeca4ed330386
Norberth <fredericus5221@gmail.com>
2019-05-14 09:45:50 +0700

    ini ketiga

--- app/src/main/java/com/netwave/indopediaid/SplashScreen.java
+++ app/src/main/java/com/netwave/indopediaid/SplashScreen.java
@@ -1,4 +1,35 @@
 package com.netwave.indopediaid;
 
-public class SplashScreen {
+import android.app.Activity;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.widget.VideoView;
+import android.os.Handler;
+
+public class SplashScreen extends Activity {
+
+    /** Duration of wait **/
+    private final int SPLASH_DISPLAY_LENGTH = 5000; //splash screen will be shown for 2 seconds
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+
+        setContentView(R.layout.bg_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent mainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
 }
